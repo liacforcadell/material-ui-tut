f07d3c5bdf40b833325b962ee70470a7502bfbb1
LIA CANO <lia.cano@gmail.com>
2021-11-19 14:45:31 -0300

    Agregando temas propios a material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import {createTheme, ThemeProvider} from '@material-ui/core' //para crear un propio tema
+import { purple } from '@material-ui/core/colors'
+
+//CREAR THEME
+const myTheme = createTheme( {
+  palette: {
+    primary: {
+      main: '#964B00'
+    },
+    secondary: purple //imports color object from material ui (implements light, dark, contrasttext, etc)
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400, 
+    fontWeightRegular: 500, 
+    fontWeightMedium: 600, 
+    fontWeightBold: 700, 
+  }
+})
+
 
 function App() {
   return (
+    <ThemeProvider theme = {myTheme}>
     <Router>
       <Switch>
         <Route exact path="/">
@@ -14,6 +35,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </ThemeProvider>
   );
 }
 
