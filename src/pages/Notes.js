0604d3b3f5bdbcb948a09263452c9f2fd5f524b5
LIA CANO <lia.cano@gmail.com>
2021-11-19 23:45:36 -0300

    Grid System

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,5 +1,9 @@
 
 import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import { Container } from '@material-ui/core'
+
 
 export default function Notes() {
 
@@ -14,10 +18,30 @@ export default function Notes() {
 
 
   return (
-    <div>
-      { notes.map(note => (        
-      <p key = {note.id}> {note.title} </p>
-      )) }
-    </div>
+    <Container>
+         {/* <Grid container>
+            <Grid item xs={12} sm={6} md={3}>  xs(Extra-small) md{} significa que todas las pantallas de medium size para arriba van a ocupar 3 columnas del grid, el grid tiene en total 12 columnas
+              <Paper>1</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Paper>2</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Paper>3</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Paper>4</Paper>
+            </Grid>
+
+         </Grid> */}
+
+         <Grid container>
+                { notes.map(note => (        
+                <Grid item key = {note.id} xs={12} md={6} lg={4}>
+                  <Paper> {note.title} </Paper>
+                </Grid>
+                )) }
+         </Grid>
+    </Container>
   )
 }
