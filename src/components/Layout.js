22fa03751c8770c3b5dc225371cf550da9fb2c54
LIA CANO <lia.cano@gmail.com>
2021-11-20 10:32:50 -0300

    Permanent Drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,23 @@
 import { makeStyles } from '@material-ui/core'
 import React from 'react'
+import Drawer from '@mui/material/Drawer';
+import Typography from '@mui/material/Typography';
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({
     page: {
         backgroundColor: '#f9f9f9',
         width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    root: {
+        display: "flex"
     }
 })
 
@@ -12,9 +25,22 @@ export default function Layout({ children }) {
     const classes = useStyles()
 
     return (
-        <div>
+        <div className = {classes.root} >
             {/* app bar */}
             {/* side drawer */}
+            <Drawer
+                className = {classes.drawer}
+                variant = "permanent"
+                anchor = "left"
+                classes = {{ paper: classes.drawerPaper}}          // allows override css build in material ui
+            
+            >
+                <div>
+                    <Typography variant="h5">
+                        Ninja Notes
+                    </Typography>
+                </div>
+            </Drawer>
 
             <div className={classes.page}>
                 {children}                
